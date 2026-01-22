46154e62969a8affd7b18d57ff125552e9d205b2
Boornima San <boornimavarna@gmail.com>
2026-01-22 09:25:47 +0530

    Practice Model Methods

--- internship_app/app/models/product.rb
+++ internship_app/app/models/product.rb
@@ -1,7 +1,26 @@
 class Product < ApplicationRecord
-    # #
-    # @status = Product.all.limit(3).pluck(:price);
-    # # @status = Product.all.limit(3).pluck(:price);
-    # @status = Product.all.limit(3).pluck(:stock);
-    # #condition for each and exucute all methods
+
+  def self.available
+    where("stock > ?", 0)
+  end
+
+  def check_availability
+    stock > 0
+  end
+
+  def apply_discount
+    case price
+    when 500..Float::INFINITY then price - 50
+    when 400...500            then price - 40
+    when 300...400            then price - 30
+    else                           price - 20
+    end
+  end
+
+  def total_amount
+    return "Product not available" unless check_availability
+    discounted_price = apply_discount
+    Math.sqrt(discounted_price**2).to_i
+  end
+
 end
